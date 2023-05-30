5fb91508af18dec518a13adcfdc8dbe3afdadb41
Luca Cassano <lucacassano96@gmail.com>
2023-05-30 09:54:58 +0200

    mail stampate

--- vue-email-list.js
+++ vue-email-list.js
@@ -5,20 +5,23 @@ createApp({
 data()
 {
     return{
-        
-       email: "",
+       emails: [],
     }
     
 },
 mounted(){
 
-    axios
+    for (i = 0; i < 10; i++) {
+        axios
         .get("https://flynn.boolean.careers/exercises/api/random/mail")
         .then(
-            risposta => {
-            console.log(risposta.data.response);
-             this.email = risposta.data.response; 
+            email => {
+            console.log(email.data.response);
+            this.emails.push(email.data.response);
         })
+        
+    }
+    
 }
 
 }).mount("#app")
